9f909bbb3a96c7670f2acea974ecdea2880d631d
01Qin <sisukasqin@hotmail.com>
2024-11-15 15:52:34 +0200

    <p>

---
+++ module 1/exercise 9/javascript 9.js
@@ -0,0 +1,22 @@
+const num = parseInt(prompt("enter a number"));
+if (num > 1) {
+    let isPrime = true;
+
+    for (let i = 2; i <= Math.sqrt(num);i++) {
+      if (num % i === 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime) {
+      alert("the number" + num+ "is a prime number.");
+    }
+
+  else{
+   alert("the number" + num+ "is not a prime number.");
+  }
+}
+else{
+    alert("the number" + num+ "is not a prime number.");
+}
+
